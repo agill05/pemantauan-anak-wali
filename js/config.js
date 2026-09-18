cf4cf73190e5d2c5d7b08032a17ac243ad59f11e
agill05 <agilabddul30@gmail.com>
2026-09-19 01:09:32 +0800

    update

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-const API_URL = "https://script.google.com/macros/s/AKfycbycxdfOFLWt0mFZAGLIr6k7u5wdSWY87rCkeNJOMuwkG2Nr5gnq_mTqF1FUzfXAoQ4/exec";
+const API_URL = "https://script.google.com/macros/s/AKfycbxK2fyrHIAPxlWPRIKhet6VGP43Kt6xe4SHAcbjSgmmx3akDPptqlFZGv4j6n63Rzwo/exec";
 
 const CACHE_TTL = 5 * 60 * 1000;
 const SNOOZE_24H_MS = 24 * 60 * 60 * 1000;
